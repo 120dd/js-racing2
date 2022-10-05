fa29e9a1641a934294a366c7861ce4cab9e898a8
Chris.c <cjs120dd@gmail.com>
2022-10-05 16:57:12 +0900

    feat:실행결과 출력

--- src/constants.js
+++ src/constants.js
@@ -3,5 +3,6 @@ export const SELECTOR = {
     CAR_NAMES_SUBMIT_BUTTON: "#car-names-submit",
     RACING_COUNT_INPUT: "#racing-count-input",
     RACING_COUNT_SUBMIT_BUTTON: "#racing-count-submit",
+    RACING_RESULT: "#racing-result",
     WINNERS: "#racing-winners",
 };
--- src/index.js
+++ src/index.js
@@ -2,9 +2,35 @@ import { $ } from './utils/dom.js';
 import { SELECTOR } from "./constants.js";
 import { Car } from "./car.js";
 
+const RESULT_CONTENTS = (carsInfo) => carsInfo.map(car => `
+<div>
+${car.name}: ${RESULT_SCORE(car.position)}
+</div>
+`).join("")
+
+const RESULT_SCORE = (_score) => {
+    let score = ""
+    for (let i = 0 ; i < _score ; i ++) {
+        score += "-"
+    }
+    return score;
+}
+
+const RESULT_TEMPLATE = (carsInfo) => `
+<br/>
+<div>
+${RESULT_CONTENTS(carsInfo)}
+</div>
+`
+
 class App {
     constructor() {
         this.cars = [];
+        this.registerCarNameSubmitEvent();
+        this.registerCountSubmitButtonEvent();
+    }
+    
+    registerCarNameSubmitEvent() {
         $(SELECTOR.CAR_NAMES_SUBMIT_BUTTON).addEventListener("click", (e) => {
             e.preventDefault();
             const inputValue = $(SELECTOR.CAR_NAMES_INPUT).value;
@@ -13,13 +39,23 @@ class App {
                 this.cars.push(new Car(v));
             });
         });
+    }
+    
+    registerCountSubmitButtonEvent() {
         $(SELECTOR.RACING_COUNT_SUBMIT_BUTTON).addEventListener("click", (e) => {
             e.preventDefault();
             const inputValue = Number($(SELECTOR.RACING_COUNT_INPUT).value);
-            this.cars.forEach(car => {
-                car.go();
-            })
+            for (let i = 0 ; i < inputValue ; i ++) {
+                this.cars.forEach(car => {
+                    car.go();
+                });
+                this.renderResult(this.cars);
+            }
         });
+    }
+    
+    renderResult(carsInfo) {
+        $(SELECTOR.RACING_RESULT).insertAdjacentHTML("beforeend", RESULT_TEMPLATE(carsInfo));
     }
 }
 
